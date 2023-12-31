2755486a0d1e927753ac6c16b3c69c4fb074f7d7
Jonay Pelluz <jonay.pelluz@mad.io>
2023-12-31 11:01:21 +0100

    feature: new backend

--- src/components/UI/BaseCard.vue
+++ src/components/UI/BaseCard.vue
@@ -12,6 +12,6 @@
     border-radius: 12px;
     box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
     padding: 0.1rem;
-    margin: 0 0 0 auto;
+    margin: 0 auto;
 }
 </style>
--- src/components/UI/BaseCentreContainer.vue
+++ src/components/UI/BaseCentreContainer.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container-fluid h-100">
         <div class="row h-100 justify-content-md-center">
-            <div class="col-12 text-center">
+            <div class="col-12 text-center box-wrapper">
                 <slot></slot>
             </div>
         </div>
